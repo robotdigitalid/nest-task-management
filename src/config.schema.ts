95c664ddaba7cbe4d195f00429f14e4567ea78f2
hqrdid <hakiramadhani@gmail.com>
2021-08-11 10:06:39 +0700

    [Auth] Add jwt secret to env

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -1,10 +1,11 @@
-import * as joi from '@hapi/joi';
+import * as Joi from '@hapi/joi';
 
-export const configValidationSchema = joi.object({
-  NODE_ENV: joi.string().required(),
-  POSTGRES_HOST: joi.string().required(),
-  POSTGRES_PORT: joi.number().default(5432).required(),
-  POSTGRES_USERNAME: joi.string().required(),
-  POSTGRES_PASSWORD: joi.string().required(),
-  POSTGRES_DATABASE: joi.string().required(),
+export const configValidationSchema = Joi.object({
+  NODE_ENV: Joi.string().required(),
+  POSTGRES_HOST: Joi.string().required(),
+  POSTGRES_PORT: Joi.number().default(5432).required(),
+  POSTGRES_USERNAME: Joi.string().required(),
+  POSTGRES_PASSWORD: Joi.string().required(),
+  POSTGRES_DATABASE: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
 });
